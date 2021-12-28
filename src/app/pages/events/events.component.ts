48d3c06013376acd978eef5240ca7304e5059a36
Shane <shanemailer@gmail.com>
2021-12-28 14:10:56 -0600

    fix(iframes): update content regularly

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent implements AfterContentInit, OnDestroy {
+  msPerHour = 3600000;
+  hours = 12 * this.msPerHour;
+  source = interval(this.hours);
+  subscription: Subscription = this.source.subscribe((val) =>
+    this.updateIframes()
+  );
+  iframes: HTMLIFrameElement[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    this.iframes = [
+      document.getElementById('event1') as HTMLIFrameElement,
+      document.getElementById('event2') as HTMLIFrameElement,
+      document.getElementById('event3') as HTMLIFrameElement,
+      document.getElementById('event4') as HTMLIFrameElement,
+    ];
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  updateIframes() {
+    this.iframes.forEach((iframe: HTMLIFrameElement) => {
+      iframe.src += '';
+    });
+  }
 }
